a58caa55002bc47777d86d57dd090766caaa11fc
oioi-code <niantoutongda@aliyun.com>
2025-08-06 13:05:41 +0800

    Update GoverDueIdentity.sol

--- GoverDueIdentity.sol
+++ GoverDueIdentity.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.28;
 
 /// This contract provides identity verification for government agencies and registered citizens.
 contract GoverDueIdentity {
