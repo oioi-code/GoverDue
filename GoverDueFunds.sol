8b61b7ba66205af7a36eed88f9311993121ef578
oioi-code <niantoutongda@aliyun.com>
2025-08-06 13:05:25 +0800

    Update GoverDueFunds.sol

--- GoverDueFunds.sol
+++ GoverDueFunds.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.28;
 
 /// This contract extends GoverDue by introducing budget allocation, expenditure tracking, and fund transfers.
 contract GoverDueFunds {
