ffb74d0258090a1993bf8ed070217ab03aa89b58
oioi-code <niantoutongda@aliyun.com>
2025-08-06 13:06:17 +0800

    Update GoverDueSurvey.sol

--- GoverDueSurvey.sol
+++ GoverDueSurvey.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.28;
 
 /// This contract enables citizens to create and participate in surveys.
 contract GoverDueSurvey {
