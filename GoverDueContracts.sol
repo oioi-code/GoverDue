ab2219974439a6cd05ed2048481539dc93e2f777
oioi-code <niantoutongda@aliyun.com>
2025-08-06 13:04:53 +0800

    Update GoverDueContracts.sol

--- GoverDueContracts.sol
+++ GoverDueContracts.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.28;
 
 /// This contract enables government agencies to create contracts, assign contractors, track progress, and release payments.
 contract GoverDueContracts {
